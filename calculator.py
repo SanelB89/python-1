9880629ce538019fbe9edaaf541125efafa132b4
SanelB89 <begichsanel@gmail.com>
2022-11-19 22:04:41 +0100

    python

---
+++ calculator.py
@@ -0,0 +1,13 @@
+
+num1 = int(input("Enter number here "))
+num2 = int(input("Enter 2nd number here "))
+
+result1 = (num1+num2)
+result2 = (num1-num2)
+result3 = (num1*num2)
+result4 = (num1%num2)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ mad libs game.py
@@ -0,0 +1,7 @@
+color = input("Enter color ")
+plural_noun = input("Enter plural noun")
+celebrity = input("Enter celebrity")
+
+print("Roses are " + color +".")
+print(plural_noun + " is blue")
+print("I love " + celebrity + ".")
